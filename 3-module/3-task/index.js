f3c47bb8a13e5663cc5617a7ca8b36568aba1b50
NZeroName <AlexanderSidorov96@yandex.ru>
2023-02-07 23:01:20 +0300

    Сделал модуль 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,6 @@
 function camelize(str) {
-  // ваш код...
+  return str
+  .split('-')
+  .map((letters, index) => index == 0 ? letters : letters[0].toUpperCase() + letters.slice(1))
+  .join('')
 }
--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,6 @@
 function showSalary(users, age) {
-  // ваш код...
+  let str = ''
+  users.map((user) => user).filter((element) => element.age <= age ? str += `${element.name}, ${element.balance}\n` : false )
+  return str.slice(0,-1)
 }
+
